c1ae2ca2e36453c245b6c41a245de1ab279e10b9
Milena <misefe1964@gmail.com>
2022-06-06 21:15:14 -0300

    adjust main c file

--- main.c
+++ main.c
@@ -16,15 +16,24 @@
 #include "digital.h"
 #include <stdint.h>
 #include "periodica.h"
-#include "timer.h"
+#include "delay.h"
+#include "uart.h"
 
 int main() {
+    SystemInit();
 
-  SystemInit();
+    UART0_Init(9600);
+    periodica_init();
+    delay_init();
+    seg_init( );
 
-  while(1)
-  {
-
-  }
- 
+    while(1){
+        uint16_t x;
+        delay_ms(1000);
+        for (uint16_t v=0;v<9999;v++){
+            printf("Entre com o numero \n");
+            scanf("%d",&x);
+            seg_apresenta(x);
+        }
+    }
 }
