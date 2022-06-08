2345f493b6ba6dcbfd954abff708ce1f8bb5b0b2
Milena <misefe1964@gmail.com>
2022-06-08 17:07:27 -0300

    adiciona enunciado e spi module

--- mcp.h
+++ mcp.h
@@ -1,13 +1,13 @@
-#ifndef _SPI_
-#define _SPI_
+#ifndef _MCP
+#define _MCP_
 #include "LPC17xx.h"
 #include "misc.h"
 
 // define pins from expansor
 
-// spi functions
+// mcp functions
 void mcp23S17_init(void);
-void mcp23S17_conf_pin(uint8_t pin, uint8_t mode);
+void mcp23S17_conf_pin(uint8_t reg_addr, uint8_t mode);
 void mcp23S17_write_pin(uint8_t reg_addr, uint8_t data);
 
 #endif
---
+++ spi.h
@@ -0,0 +1,11 @@
+#ifndef _SPI_
+#define _SPI_
+#include "LPC17xx.h"
+#include "misc.h"
+
+// define pins from expansor
+
+// spi functions
+spi_write(uint8_t byte);
+
+#endif
