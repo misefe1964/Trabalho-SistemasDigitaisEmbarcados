5d4d77901706df751fdb170167a8a03c6c492fe8
cadu08 <carlos.ceaf08@gmail.com>
2022-06-11 15:32:51 -0300

    add mcp23s17 library file

--- mcp23S17.h
+++ mcp23S17.h
@@ -1,7 +1,27 @@
 #ifndef _MCP23S17_
 #define _MCP23S17_
 
-#define IODIRA 0x00
-#define IODIRB 0x10
+#define IODIRA     0x00
+#define IODIRB     0x10
+#define IPOLA      0x01
+#define IPOLB      0x11
+#define GPINTENA   0x02
+#define GPINTENB   0x12
+#define DEFVALA    0x03
+#define DEFVALB    0x13
+#define INTCONA    0x04
+#define INTCONB    0x14
+#define IOCON1     0x05
+#define IOCON2     0x15
+#define GPPUA      0x06
+#define GPPUB      0x16
+#define INTFA      0x07
+#define INTFB      0x17
+#define INTCAPA    0x08
+#define INTCAPB    0x18
+#define GPIOA      0x09
+#define GPIOB      0x19
+#define OLATA      0x0A
+#define OLATB      0x1A
 
 #endif
