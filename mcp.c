6a0af65a7b664dff0b5b2609b0e5d0943416f045
Milena <misefe1964@gmail.com>
2022-06-13 19:43:55 -0300

    create current_GPIOA var

--- mcp.c
+++ mcp.c
@@ -1,6 +1,7 @@
 #include "mcp.h"
 
-uint8_t current_IODIRA = 0xFF;
+uint8_t current_IODIRA;
+uint8_t current_GPIOA;
 
 void mcp23S17_init(void) {
     // set registers
@@ -28,9 +29,15 @@ void mcp23S17_init(void) {
     LPC_SPI->SPCR &= nbit4;   // bit is sent on rising edge and sampled on falling edge
 
     // All IO pins start as INPUT
+    current_IODIRA = 0xFF;
     spi_write(0x41);
     spi_write(IODIRA);
     spi_write(current_IODIRA);
+    // All IO pins start as LOW
+    current_GPIOA = 0x00;
+    spi_write(0x41);
+    spi_write(IODIRA);
+    spi_write(current_GPIOA);
 }
 
 void mcp23S17_conf_pin(uint8_t pin, uint8_t mode) {
@@ -42,16 +49,20 @@ void mcp23S17_conf_pin(uint8_t pin, uint8_t mode) {
     spi_write(IODIRA);
     // update current state of IODIRA register
     if (mode == INPUT)
-        current_IODIRA |= (mode << pin);
+        current_IODIRA |= (1 << pin);
     else if (mode == OUTPUT)
-        current_IODIRA &= (mode << pin);
+        current_IODIRA &= (0 << pin);
     spi_write(current_IODIRA);
 
     set_cs_high();
 }
 
 uint8_t mcp23S17_write_pin(uint8_t pin, uint8_t data, uint8_t mode) {
+    if (data == 1) current_GPIOA |= (1 << pin)
+    else if (data == 0) current_GPIOA &= (0 << pin)
+
     set_cs_low();
+
     // send device opcode = 0100 0000 + mode
     spi_write(0x40 + mode);
 
@@ -59,7 +70,8 @@ uint8_t mcp23S17_write_pin(uint8_t pin, uint8_t data, uint8_t mode) {
     spi_write(GPIOA);
 
     // set data to be written
-    uint8_t b = spi_write(data);
+    uint8_t b = spi_write(current_GPIOA);
+
     set_cs_high();
 
     if(mode == READ) {
