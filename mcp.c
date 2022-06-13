7a6a62f5d9e4856dc0adce36fb2b88f6a556d733
Milena Seibert Fernandes <43688094+misefe1964@users.noreply.github.com>
2022-06-13 20:07:06 -0300

    feat: mcp write function implementation (#6)
    * implementation of write pin function for expansor
    
    * further adjustments
    
    * further adjustments on mcp functions
    
    * create current_GPIOA var
    
    Co-authored-by: Carlos Ferreira <89271917+cadu08@users.noreply.github.com>

--- mcp.c
+++ mcp.c
@@ -1,6 +1,7 @@
 #include "mcp.h"
 
-uint8_T current_IODIRA;
+uint8_t current_IODIRA;
+uint8_t current_GPIOA;
 
 void mcp23S17_init(void) {
     // set registers
@@ -32,18 +33,52 @@ void mcp23S17_init(void) {
     spi_write(0x41);
     spi_write(IODIRA);
     spi_write(current_IODIRA);
+    // All IO pins start as LOW
+    current_GPIOA = 0x00;
+    spi_write(0x41);
+    spi_write(IODIRA);
+    spi_write(current_GPIOA);
 }
 
 void mcp23S17_conf_pin(uint8_t pin, uint8_t mode) {
+    set_cs_low();
+
     // send device opcode = 0100 0001
     spi_write(0x41);
     // send register address
     spi_write(IODIRA);
     // update current state of IODIRA register
-    current_IODIRA = current_IODIRA | (1 << pin);
-    b = spi_write(current_IODIRA);
+    if (mode == INPUT)
+        current_IODIRA |= (1 << pin);
+    else if (mode == OUTPUT)
+        current_IODIRA &= (0 << pin);
+    spi_write(current_IODIRA);
+
+    set_cs_high();
 }
 
-void mcp23S17_write_pin(uint8_t reg_addr, uint8_t data) {
+uint8_t mcp23S17_write_pin(uint8_t pin, uint8_t data, uint8_t mode) {
+    if (data == 1) current_GPIOA |= (1 << pin)
+    else if (data == 0) current_GPIOA &= (0 << pin)
+
+    set_cs_low();
+
+    // send device opcode = 0100 0000 + mode
+    spi_write(0x40 + mode);
+
+    // send register address
+    spi_write(GPIOA);
+
+    // set data to be written
+    uint8_t b = spi_write(current_GPIOA);
+
+    set_cs_high();
+
+    if(mode == READ) {
+        return b;
+    }
+    else if (mode == WRITE) {
+        return (uint8_t)0;
+    }
 }
 
--- mcp.h
+++ mcp.h
@@ -2,13 +2,16 @@
 #define _MCP_
 #include "LPC17xx.h"
 #include "misc.h"
+#include "spi.h"
+#include "mcp23S17.h"
 
 #define OUTPUT 0
 #define INPUT 1
+#define WRITE 0
+#define READ 1
 
 void mcp23S17_init(void);
-void mcp23S17_conf_pin(uint8_t reg_addr, uint8_t mode);
-void mcp23S17_write_pin(uint8_t reg_addr, uint8_t data);
-
+void mcp23S17_conf_pin(uint8_t pin, uint8_t mode);
+uint8_t mcp23S17_write_pin(uint8_t pin, uint8_t data, uint8_t mode);
 #endif
 
