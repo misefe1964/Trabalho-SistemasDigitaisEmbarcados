67c24cd3e58c9e6f0db693e9c4f32aea2bedaf34
Carlos Ferreira <89271917+cadu08@users.noreply.github.com>
2022-06-15 17:35:37 -0300

    chore: implement spi_init and mcp_init functions (#9)
    * remove master registers from mcp_init
    
    * Configuring SPCCR registrator
    
    * Add spi_init function to header file
    
    * resolving github comments
    
    * Update spi.c
    
    Co-authored-by: Milena Seibert Fernandes <43688094+misefe1964@users.noreply.github.com>
    
    * add blank line at the end of mcp.h
    
    Co-authored-by: Milena Seibert Fernandes <43688094+misefe1964@users.noreply.github.com>

--- spi.c
+++ spi.c
@@ -1,6 +1,43 @@
 #include "spi.h"
 
-uint8_t spi_write(uint8_t byte){
+static uint32_t spi_GetPclk(void);
+
+void spi_init() {
+    pinMode(MOSI,OUTPUT);
+    pinMode(MISO,INPUT);
+    pinMode(SCK,OUTPUT);
+    pinMode(SSEL,OUTPUT);
+
+    // set registers
+    LPC_SC->PCONP |= bit8; // power on PCSPI
+    LPC_SC->PCLKSEL0 |= bit16;
+    LPC_SC->PCLKSEL0 |= bit17; // select clock for SPI
+
+    // PINSEL0[31:30] = 11, PSI Clock = SCK
+    LPC_PINCON->PINSEL0 |= bit30; 
+    LPC_PINCON->PINSEL0 |= bit31;
+    // PINMODE
+    // PINSEL1
+    LPC_PINCON->PINSEL1 |= bit0;
+    LPC_PINCON->PINSEL1 |= bit1; // select SSEL
+
+    LPC_PINCON->PINSEL1 |= bit2;
+    LPC_PINCON->PINSEL1 |= bit3; // select MISO
+
+    LPC_PINCON->PINSEL1 |= bit4;
+    LPC_PINCON->PINSEL1 |= bit5; // select MOSI
+
+    //S0SPINT
+    LPC_SPI->SPINT &= nbit0; // interrupt flag is not set
+
+    LPC_SPI->SPCR |= bit3; 
+    LPC_SPI->SPCR &= nbit4;   // bit is sent on rising edge and sampled on falling edge
+
+    LPC_SPI->SPCCR = spi_GetPclk()/SCK_Freq; // Set Spi Clock
+  
+}
+
+uint8_t spi_write(uint8_t byte) {
     uint8_t valor = 0;
     uint8_t bit;
     for(uint8_t x = 0; x <8; x++) {
@@ -21,4 +58,25 @@ void set_cs_low() {
 
 void set_cs_high() {
     digitalWrite(SSEL, HIGH);
+}
+
+static uint32_t spi_GetPclk(void) {
+    uint32_t v_spiPclk_u32,v_Pclk_u32;
+    v_spiPclk_u32 = (LPC_SC->PCLKSEL0 >> 16) & 0x03;
+    switch( v_spiPclk_u32 )
+    {
+    case 0x00:
+        v_Pclk_u32 = SystemCoreClock/4;           //SystemFrequency or  SystemCoreClock
+        break;
+    case 0x01:
+        v_Pclk_u32 = SystemCoreClock;
+        break;
+    case 0x02:
+        v_Pclk_u32 = SystemCoreClock/2;
+        break;
+    case 0x03:
+        v_Pclk_u32 = SystemCoreClock/8;
+        break;
+    }
+    return (v_Pclk_u32);
 }
--- spi.h
+++ spi.h
@@ -5,16 +5,19 @@
 #include "digital.h"
 #include "delay.h"
 
-#define SCK PIN(0, 15)
+#define SCK  PIN(0, 15)
 #define SSEL PIN(0, 16)
 #define MISO PIN(0, 17)
 #define MOSI PIN(0, 18)
 
+#define SCK_Freq 4000000
+
 // spi functions
 uint8_t spi_write(uint8_t byte);
+
+void spi_init();
 
 void set_cs_low();
 void set_cs_high();
 
 #endif
-
