09d3d5c69d344a8c94993597cc0e82f35ecfd26c
Carlos Ferreira <89271917+cadu08@users.noreply.github.com>
2022-07-24 14:36:07 -0300

    Chore: Unifying transmitter and receptor code into main.c (#15)
    * unifying transmitter and receptor code
    
    * deleting testaR_nrf24.c
    
    * deleting testaT_nrf24.c
    
    * putting common resources as global

--- TRABALHO2/main.c
+++ TRABALHO2/main.c
@@ -1,12 +1,36 @@
-includes
+#include "LPC17xx.h"
+#include "uart.h"
+#include "digital.h"
+#include <stdio.h>
+#include <string.h>
+#include "nrf24.h"
+#include "delay.h"
 
+SystemInit();
+delay_init();
+uart_init();
+nrf24_init();
+
+uint8_t buffer[10];
 int modulo_receptor = 1
 
 void main {
-    if (modulo_receptor) {
-        // cod receptor
+  if (modulo_receptor) {
+    printf("Rodando receptor...\n");
+    while(1) {
+      //codigo para o receptor
+      nrf24_recebe(buffer);
+      buffer[TAMANHO_MSG]=0;
+      printf("recebido=%s\n",(char*)buffer);
     }
-    else {
-        // cod transmissor
+  }
+  else {
+    uint16_t x = 0;
+    printf("Rodando transmissor...\n");
+    while(1) {
+      sprintf((char *)buffer, "oi %d",x);
+      nrf24_transmite(buffer); x++;
+      delay_ms(100);
     }
+  }
 }
